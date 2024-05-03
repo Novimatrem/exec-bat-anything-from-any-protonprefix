b0d2c553ffbe0e124d8c631327e56730907aa1ac
zoeyglobe <thenovimatrem@protonmail.ch>
2024-05-03 11:03:50 +0000

    fixes, and comments, to help.

--- epic-princess-to-epic-games-store-slash-launcher/exec.bat
+++ epic-princess-to-epic-games-store-slash-launcher/exec.bat
@@ -1,5 +1,19 @@
 @echo off
+
+REM requires start.exe, msiexec.exe, and cmd.exe from your wine system install,
+REM place those in the game folder. replace Epic_Princess.exe with cmd.exe (renamed to that).
+REM put the newest epic games store installer in the game folder alongside those.
+REM then run the game and in the command line: start msiexec /i EpicInstaller-15.17.1.msi
+REM install epic games store to C:\Epic Games exactly (pick C:\ when it asks Browse)
+REM then put this script in the folder, run Epic Princess again, and type exec
+REM if the installer ever pops up asking you to Repair, do it.
+REM profit.
+
 echo bongo
+
+dir
+echo nowwork
+start msiexec /i EpicInstaller-15.17.1.msi
 cd C:\
 
 
@@ -10,7 +24,7 @@ dir
 whoami
 
 for %%I in (.) do echo %%~nxI
-
+echo test1
 cd compatdata
 cd 2195670
 cd pfx
@@ -34,12 +48,15 @@ cd ..
 REM -----
 echo tim
 for %%I in (.) do echo %%~nxI
-
+cd Epic Games
 cd Launcher
-cd Engine
+cd Portal
 cd Binaries
-cd Win64
+cd Win32
+for %%I in (.) do echo %%~nxI
+dir
 EpicGamesLauncher.exe
 
 echo done
 exit
+pause
